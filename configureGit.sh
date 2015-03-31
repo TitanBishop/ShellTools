9dcf99bf6d73643f8c817814a106f7d6ca103ea9
TitanBishop <bishop.titan@gmail.com>
2015-03-31 10:47:04 -0500

    git config script

--- configureGit.sh
+++ configureGit.sh
@@ -10,3 +10,6 @@ git config --global user.name "$USER"
 git config --system user.name "$USER"
 git config --global user.email "$EMAIL"
 git config --system user.email "$EMAIL"
+
+git config --global credential.helper cache
+git config --system credential.helper cache
