91fe151f724c7eeab4764e9c8852f04e4f9e69d9
TitanBishop <bishop.titan@gmail.com>
2015-03-31 10:45:14 -0500

    git user and email config script

---
+++ configureGit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "What is your github email?"
+read EMAIL
+
+echo "What is your github username?" 
+read USER
+
+git config --global user.name "$USER"
+git config --system user.name "$USER"
+git config --global user.email "$EMAIL"
+git config --system user.email "$EMAIL"
